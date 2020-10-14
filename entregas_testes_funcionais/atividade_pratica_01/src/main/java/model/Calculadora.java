d661f9f074924b1f4a7d36f5fd7981c4437bd64d
Jonatas dos Reis <jonatasdrf@gmail.com>
2020-10-13 22:48:44 -0300

    commit entregas_testes_funcionais

---
+++ entregas_testes_funcionais/atividade_pratica_01/src/main/java/model/Calculadora.java
@@ -0,0 +1,11 @@
+package model;
+
+import org.json.JSONObject;
+
+public class Calculadora {
+
+    //Método para calcular a porcentagem de um valor total
+    public double parametroPorcentagem(String json){
+        return 10;
+    }
+}
