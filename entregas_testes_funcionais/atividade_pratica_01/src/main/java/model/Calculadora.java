dba0276c1d71cdefa3a6bedd66520b1a4667aaa8
Jonatas dos Reis <jonatasdrf@gmail.com>
2020-10-13 22:51:12 -0300

    atividade_pratica_01 estado verde

--- entregas_testes_funcionais/atividade_pratica_01/src/main/java/model/Calculadora.java
+++ entregas_testes_funcionais/atividade_pratica_01/src/main/java/model/Calculadora.java
@@ -6,6 +6,14 @@ public class Calculadora {
 
     //Método para calcular a porcentagem de um valor total
     public double parametroPorcentagem(String json){
-        return 10;
+        JSONObject percent = new JSONObject(json);
+        double valorTotal = percent.getDouble("valorTotal");
+        double porcentagem = percent.getDouble("porcentagem");
+        if(porcentagem >= 0 && porcentagem <= 100){
+            return (valorTotal * porcentagem) / 100;
+        }
+        else{
+            return 0;
+        }
     }
 }
