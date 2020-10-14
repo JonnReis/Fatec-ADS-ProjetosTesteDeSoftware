4e3c6228710a93f193955650e3ba444a6d88aab0
Jonatas dos Reis <jonatasdrf@gmail.com>
2020-10-13 22:43:57 -0300

    TDD_estado verde

--- entrega_introducao/src/main/java/model/Calculadora.java
+++ entrega_introducao/src/main/java/model/Calculadora.java
@@ -6,11 +6,19 @@ public class Calculadora {
 
     //Método para calcular a porcentagem de um valor total
     public double parametroPorcentagem(String json){
-      return 0;
+        JSONObject percent = new JSONObject(json);
+        double valorTotal = percent.getDouble("valorTotal");
+        double porcentagem = percent.getDouble("porcentagem");
+        double res = (valorTotal * porcentagem) / 100;
+        return res;
     }
 
     //Método para calcular a porcentagem correspondente de um valor final sobre um valor total
     public double parametroValorFinal(String json) {
-     return 0;
+        JSONObject total = new JSONObject(json);
+        double resultado = total.getDouble("resultado");
+        double valorTotal = total.getDouble("valorTotal");
+        double result = (resultado * 100) / valorTotal;
+        return result;
     }
 }
