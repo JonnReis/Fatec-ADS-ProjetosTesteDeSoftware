129a452a4e6459f6a7f813996485b3420facbe56
Jonatas dos Reis <jonatasdrf@gmail.com>
2020-10-13 22:53:41 -0300

    atividade_pratica_01 estado azul

--- entregas_testes_funcionais/atividade_pratica_01/src/main/java/model/Calculadora.java
+++ entregas_testes_funcionais/atividade_pratica_01/src/main/java/model/Calculadora.java
@@ -9,11 +9,7 @@ public class Calculadora {
         JSONObject percent = new JSONObject(json);
         double valorTotal = percent.getDouble("valorTotal");
         double porcentagem = percent.getDouble("porcentagem");
-        if(porcentagem >= 0 && porcentagem <= 100){
-            return (valorTotal * porcentagem) / 100;
-        }
-        else{
-            return 0;
-        }
+        if(porcentagem >= 0 && porcentagem <= 100) return (valorTotal * porcentagem) / 100;
+        else return 0;
     }
 }
